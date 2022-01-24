37d5fe797e97e5429f7dccc908ddeced13c11ac1
Marwan-Takrouri <monitak2000@gmail.com>
2022-01-23 21:28:48 -0500

    final update

--- PyPoll.py
+++ PyPoll.py
@@ -70,7 +70,7 @@
 import csv
 import os
  # Assign a variable to load a file from a path.
-file_to_load = os.path.join("Resources", "election_results.csv")
+file_to_load = os.path.join("resources", "election_results.csv")
  # Assign a variable to save the file to a path.
 file_to_save = os.path.join("analysis", "election_analysis.txt")
 
